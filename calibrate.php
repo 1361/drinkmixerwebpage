3dc8bea7386e1e36b796d855a50a10467ec26d92
1361 <andrew.minarick@gmail.com>
2018-04-03 17:45:09 -0500

    Add files via upload

--- calibrate.php
+++ calibrate.php
@@ -1,42 +1,272 @@
 
 <html>
 
-<br></br>
-<a href='index.html'><button>Home Page</button></a>
-<br></br>
-<a href='mixdrinks.php'><button>Mix Drinks</button></a>
-<br></br>
+</html>
 
-
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="http://html5-templates.com/" />
+    <title>Automated Drink Mixer</title>
+	<meta name="description" content="A minimalist Bootstrap theme by StartBootstrap. Contains everything you need to get started building your website. All you have to do is change the text and images.">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
 
 <body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Start Mixing</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href='calibrate.php'>Calibrate Drink Mixer</a>
+		    </li>
+		    <li>
+			<a href='mixdrinks.php'>Mix Drinks</a>
+
+		    </li>
+                    <li>
+                        <a href="services.html">About</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                    
+                   
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+<body> 
+<form action='mixdrinks.php' method='post'> 
 
 <br></br>
-<p>Select the beverage type  position 1 </p>
+ <div class="col-md-4">
+		
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Select the beverage type  position 1 (Mixers Only)</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<select name= 'beverage1'>
+		        <option value='none' selected>None</option>
+		        <option value='sevenup'>SevenUp</option>
+		        <option value='coke'>Coke</option>
+	                </select>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+<br></br>
+   
+<br></br>
+ <div class="col-md-4">
+		
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Select the beverage type  position 2</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<select name= 'beverage2'>
+                		<option value='none' selected>None</option>
+                		<option value='whiskey'>Whiskey</option>
+                		<option value='vodka'>Vodka</option>
+				<option value='rum'>Rum</option>
+				<option value='tequila'>Tequila</option>
+        		</select>
+	                </select>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+<br></br>
+
+
+<br></br>
+ <div class="col-md-4">
+		
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Select the beverage type  position 3</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<select name= 'beverage3'>
+                		<option value='none' selected>None</option>
+                		<option value='whiskey'>Whiskey</option>
+                		<option value='vodka'>Vodka</option>
+				<option value='rum'>Rum</option>
+				<option value='tequila'>Tequila</option>
+        		</select>
+	                </select>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+<br></br>
+
+<br></br>
+ <div class="col-md-4">
+		
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Select the beverage type  position 4</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<select name= 'beverage4'>
+                		<option value='none' selected>None</option>
+                		<option value='whiskey'>Whiskey</option>
+                		<option value='vodka'>Vodka</option>
+				<option value='rum'>Rum</option>
+				<option value='tequila'>Tequila</option>
+        		</select>
+	                </select>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+<br></br>
+
+<br></br>
+ <div class="col-md-4">
+		
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Select the beverage type  position 5</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<select name= 'beverage5'>
+                		<option value='none' selected>None</option>
+                		<option value='whiskey'>Whiskey</option>
+                		<option value='vodka'>Vodka</option>
+				<option value='rum'>Rum</option>
+				<option value='tequila'>Tequila</option>
+        		</select>
+	                </select>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+<br></br>
+
+<br></br>
+ <div class="col-md-4">
+		
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Select the beverage type  position 6</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<select name= 'beverage6'>
+                		<option value='none' selected>None</option>
+				<option value='sevenup'>SevenUp</option>
+				<option value='coke'>Coke</option>
+        		</select>
+	                </select>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+<br></br>
+
+
+
+
+<!--
+
 
 <form action='' method='post'>
+<br></br>
+<p>Select the beverage type  position 1 (Mixers Only)</p>
 	<select name= 'beverage1'>
 		<option value='none' selected>None</option>
-		<option value='whiskey'>Whiskey</option>
+		<option value='sevenup'>SevenUp</option>
 		<option value='coke'>Coke</option>
 	</select>
-	<input type='submit'>
-</form>
 <br></br>
 
-<br></br>
 <p>Select the beverage type  position 2 </p>
-
-<form action='' method='post'>
         <select name= 'beverage2'>
                 <option value='none' selected>None</option>
                 <option value='whiskey'>Whiskey</option>
-                <option value='coke'>Coke</option>
+                <option value='vodka'>Vodka</option>
+		<option value='rum'>Rum</option>
+		<option value='tequila'>Tequila</option>
         </select>
-	<input type='submit'>
-</form>
 <br></br>
 
+<p>Select the beverage type  position 3 </p>
+        <select name= 'beverage3'>
+                <option value='none' selected>None</option>
+                <option value='whiskey'>Whiskey</option>
+                <option value='vodka'>Vodka</option>
+		<option value='rum'>Rum</option>
+		<option value='tequila'>Tequila</option>
+        </select>
+<br></br>
+
+<p>Select the beverage type  position 4 </p>
+        <select name= 'beverage4'>
+                <option value='none' selected>None</option>
+                <option value='whiskey'>Whiskey</option>
+                <option value='vodka'>Vodka</option>
+		<option value='rum'>Rum</option>
+		<option value='tequila'>Tequila</option>
+        </select>
+<br></br>
+
+<p>Select the beverage type  position 5 </p>
+        <select name= 'beverage5'>
+                <option value='none' selected>None</option>
+                <option value='whiskey'>Whiskey</option>
+                <option value='vodka'>Vodka</option>
+		<option value='rum'>Rum</option>
+		<option value='tequila'>Tequila</option>
+        </select>
+<br></br>
+
+<p>Select the beverage type  position 6 (Mixers Only) </p>
+        <select name= 'beverage6'>
+                <option value='none' selected>None</option>
+                <option value='coke'>Coke</option>
+                <option value='sevenup'>SevenUp</option>
+        </select>
+<br></br>
+-->
+<br></br>
+<input type='submit'>
+</form>
 
 </body>
 </html>
@@ -45,16 +275,16 @@
 switch ($_POST[beverage1]){
 	case 'none':
 		break;
-	case 'whiskey':
-		$position1a = 37;
-		$position1 = json_encode($position1a);
-		$file = '/tmp/whiskey';
-		file_put_contents($file, $position1);
-		break;
 	case 'coke' :
 		$position1a = 37;
                 $position1 = json_encode($position1a);
                 $file = '/tmp/coke';
+                file_put_contents($file, $position1);
+                break;
+	case 'sevenup' :
+		$position1a = 37;
+                $position1 = json_encode($position1a);
+                $file = '/tmp/sevenup';
                 file_put_contents($file, $position1);
                 break;
 
@@ -69,16 +299,130 @@ switch ($_POST[beverage2]){
                 $file = '/tmp/whiskey';
                 file_put_contents($file, $position2);
                 break;
-        case 'coke' :
+        case 'vodka':
                 $position2a = 35;
                 $position2 = json_encode($position2a);
-                $file = '/tmp/coke';
+                $file = '/tmp/vodka';
                 file_put_contents($file, $position2);
+                break;
+	case 'rum':
+                $position2a = 35;
+                $position2 = json_encode($position2a);
+                $file = '/tmp/rum';
+                file_put_contents($file, $position2);
+                break;
+	case 'tequila':
+                $position2a = 35;
+                $position2 = json_encode($position2a);
+                $file = '/tmp/tequila';
+                file_put_contents($file, $position2);
+                break;
+}
+
+switch ($_POST[beverage3]){
+        case 'none':
+                break;
+        case 'whiskey':
+                $position3a = 33;
+                $position3 = json_encode($position3a);
+                $file = '/tmp/whiskey';
+                file_put_contents($file, $position3);
+                break;
+        case 'vodka':
+                $position3a = 33;
+                $position3 = json_encode($position3a);
+                $file = '/tmp/vodka';
+                file_put_contents($file, $position3);
+                break;
+	case 'rum':
+                $position3a = 33;
+                $position3 = json_encode($position3a);
+                $file = '/tmp/rum';
+                file_put_contents($file, $position3);
+                break;
+	case 'tequila':
+                $position3a = 33;
+                $position3 = json_encode($position3a);
+                $file = '/tmp/tequila';
+                file_put_contents($file, $position3);
+                break;
+}
+
+switch ($_POST[beverage4]){
+        case 'none':
+                break;
+        case 'whiskey':
+                $position4a = 31;
+                $position4 = json_encode($position4a);
+                $file = '/tmp/whiskey';
+                file_put_contents($file, $position4);
+                break;
+        case 'vodka':
+                $position4a = 31;
+                $position4 = json_encode($position4a);
+                $file = '/tmp/vodka';
+                file_put_contents($file, $position4);
+                break;
+	case 'rum':
+                $position4a = 31;
+                $position4 = json_encode($position4a);
+                $file = '/tmp/rum';
+                file_put_contents($file, $position4);
+                break;
+	case 'tequila':
+                $position4a = 31;
+                $position4 = json_encode($position4a);
+                $file = '/tmp/tequila';
+                file_put_contents($file, $position4);
+                break;
+}
+
+switch ($_POST[beverage5]){
+        case 'none':
+                break;
+        case 'whiskey':
+                $position5a = 29;
+                $position5 = json_encode($position5a);
+                $file = '/tmp/whiskey';
+                file_put_contents($file, $position5);
+                break;
+        case 'vodka':
+                $position5a = 29;
+                $position5 = json_encode($position5a);
+                $file = '/tmp/vodka';
+                file_put_contents($file, $position5);
+                break;
+	case 'rum':
+                $position5a = 29;
+                $position5 = json_encode($position5a);
+                $file = '/tmp/rum';
+                file_put_contents($file, $position5);
+                break;
+	case 'tequila':
+                $position5a = 29;
+                $position5 = json_encode($position5a);
+                $file = '/tmp/tequila';
+                file_put_contents($file, $position5);
+                break;
+}
+
+switch ($_POST[beverage6]){
+	case 'none':
+		break;
+	case 'coke' :
+		$position6a = 40;
+                $position6 = json_encode($position6a);
+                $file = '/tmp/coke';
+                file_put_contents($file, $position6);
+                break;
+	case 'sevenup' :
+		$position6a = 40;
+                $position6 = json_encode($position6a);
+                $file = '/tmp/sevenup';
+                file_put_contents($file, $position6);
                 break;
 
 }
-
-
 
 
 ?>
--- mixdrinks.php
+++ mixdrinks.php
@@ -1,28 +1,192 @@
-<html>
 
-<br></br>
-<a href='index.html'><button>Home Page</button></a>
-<br></br>
-<a href='calibrate.php'><button>Calibrate Mixer</button></a>
-<br></br>
-<br></br>
+
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="http://html5-templates.com/" />
+    <title>Automated Drink Mixer</title>
+	<meta name="description" content="A minimalist Bootstrap theme by StartBootstrap. Contains everything you need to get started building your website. All you have to do is change the text and images.">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
 
 <body>
-<form method='post'> 
-<input type='submit' value='Mix Drink' name='wc'> Whiskey and Coke </input>
-</form>
-<br></br>
-</body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Start Mixing</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href='calibrate.php'>Calibrate Drink Mixer</a>
+		    </li>
+		    <li>
+			<a href='mixdrinks.php'>Mix Drinks</a>
+
+		    </li>
+                    <li>
+                        <a href="services.html">About</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                    
+                   
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+   
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Mix Drinks
+                </h1>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Whiskey Coke</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<form method='post'> 
+			<input type='submit' value='Mix Drink' name='wc'></input>
+			</form>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
 
 
 
-</html>
+	    <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Whiskey and Seven</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<form method='post'> 
+			<input type='submit' value='Mix Drink' name='ws'></input>
+			</form>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+	
+
+
+	    <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Rum and Coke</h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<form method='post'> 
+			<input type='submit' value='Mix Drink' name='rc'></input>
+			</form>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+	
+
+
+	    <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Vodka and Seven </h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<form method='post'> 
+			<input type='submit' value='Mix Drink' name='vs'></input>
+			</form>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+	
+
+
+	    <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Tequila Shot </h4>
+                    </div>
+                    <div class="panel-body">
+                        <body>
+			<form method='post'> 
+			<input type='submit' value='Mix Drink' name='ts'></input>
+			</form>
+			<br></br>
+			</body>
+                    </div>
+                </div>
+            </div>
+	
+
+            
+        </div>
+        <!-- /.row -->
+
+
+
 
 
 <?php 
 if ($_POST[wc]){
         $result1 = shell_exec('sudo python /home/pi/whiskey.py & sudo python /home/pi/coke.py &');
-	echo('Whiskey Coke Mixed');
+	echo('Whiskey and Coke Mixed');
+}
+
+if ($_POST[ws]){
+        $result1 = shell_exec('sudo python /home/pi/whiskey.py & sudo python /home/pi/sevenup.py &');
+	echo('Whiskey and SevenUp Mixed');
+}
+
+if ($_POST[rc]){
+        $result1 = shell_exec('sudo python /home/pi/rum.py & sudo python /home/pi/coke.py &');
+	echo('Rum and Coke Mixed');
+}
+
+if ($_POST[vs]){
+        $result1 = shell_exec('sudo python /home/pi/vodka.py & sudo python /home/pi/sevenup.py &');
+	echo('Vodka and SevenUp Mixed');
+}
+
+if ($_POST[ts]){
+        $result1 = shell_exec('sudo python /home/pi/tequila.py');
+	echo('Tequila Trevor Initiated');
 }
 
 
